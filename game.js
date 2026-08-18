f8f35cb3f3c25880a0bfba45582398d1274676d1
kesche07 <keschellap@gmail.com>
2026-08-18 23:10:28 +0545

    initial screen set up

--- game.js
+++ game.js
@@ -1,1 +1,8 @@
-const canvas = document.querySelector('canvas');
+const canvas = document.querySelector('canvas')
+const c = canvas.getContext('2d')
+
+//game screen size 16:9 ratio
+canvas.width = 1024
+canvas.height = 576
+
+c.fillRect(0,0,canvas.width,canvas.height)
