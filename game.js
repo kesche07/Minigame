7853c0cfe0b259449dfe6c20742af98e51ee8f0e
kesche07 <keschellap@gmail.com>
2026-08-24 22:32:47 +0545

    Added changes to responsiveness and avoided player overflow

--- game.js
+++ game.js
@@ -274,6 +274,19 @@ function animate(){
         enemy.switchSprite('fall')
     }
 
+    if (player.position.x < 0) {
+    player.position.x = 0
+} else if (player.position.x + player.width > canvas.width) {
+    player.position.x = canvas.width - player.width
+}
+
+// Clamp Enemy inside screen
+if (enemy.position.x < 0) {
+    enemy.position.x = 0
+} else if (enemy.position.x + enemy.width > canvas.width) {
+    enemy.position.x = canvas.width - enemy.width
+}
+
     
     
     //if player is attacking detection && hits enemy
@@ -310,7 +323,6 @@ function animate(){
         {
             player.takeHit()
             enemy.isAttacking = false
-            player.health -= 20
             document.querySelector('#playerhealth').style.width= player.health + '%'
         }
 
